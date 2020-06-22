16c6a11431ec6cd388e6477703048fb03b0e8d11
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-06-22 17:19:56 +0500

    Update Calculator.js

--- src/Components/Calculator/Calculator.js
+++ src/Components/Calculator/Calculator.js
@@ -8,9 +8,9 @@ import { Keypad } from './Keypad';
 export const Calculator = () => {
     const { showCalculator } = useContext(GlobalContext);
     
+    // Set the className to change the visibility of calculator 
+    // depending on the value of 'showCalculator' (true/false).
     return (
-        // Set the className to change the visibility of calculator 
-        // depending on the value of 'showCalculator' (true/false).
         <div className={`Calculator ${showCalculator ? 'show' : 'hide'}`}>
             <CalcHead />
             <Screen />
