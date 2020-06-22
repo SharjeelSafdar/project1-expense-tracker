8d15270ad84364d0c03e4b3f925d8e47ee741c1a
SharjeelSafdar <SharjeelSafdar@users.noreply.github.com>
2020-06-22 22:00:26 +0500

    Update AccountSummary.js
    Restricted the decimal places to 2 digits

--- src/Components/AccountSummary.js
+++ src/Components/AccountSummary.js
@@ -17,21 +17,21 @@ export const AccountSummary = () => {
             <div className="balance">
                 <div className="balanceImage"><img src="/images/balance.png" alt="Current Balance"/></div>
                 <div>
-                    <h2>{`${total<0 ? '-' : ''}$${Math.abs(total)}`}</h2>
+                    <h2>{`${total<0 ? '-' : ''}$${Math.abs(total.toFixed(2))}`}</h2>
                     <h4>Current Balance</h4>
                 </div>
             </div>
             <div className="income">
                 <div className="incomeImage"><img src="/images/income.png" alt="Current Balance"/></div>
                 <div>
-                    <h2>{`${income<0 ? '-' : ''}$${Math.abs(income)}`}</h2>
+                    <h2>{`${income<0 ? '-' : ''}$${Math.abs(income.toFixed(2))}`}</h2>
                     <h4>Total Income</h4>
                 </div>
             </div>
             <div className="expense">
                 <div className="expenseImage"><img src="/images/expense.png" alt="Current Balance"/></div>
                 <div>
-                    <h2>{`${expense<0 ? '-' : ''}$${Math.abs(expense)}`}</h2>
+                    <h2>{`${expense<0 ? '-' : ''}$${Math.abs(expense.toFixed(2))}`}</h2>
                     <h4>Total Expenditure</h4>
                 </div>
             </div>
